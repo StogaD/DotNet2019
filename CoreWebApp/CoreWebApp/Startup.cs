45f7517606dfe94f0f7e24fbb6e6cefb173cf2ff
stogad <dstoga@pgs-soft.com>
2019-10-04 12:04:48 +0200

    Serilog: Added logging to Console and File

--- CoreWebApp/CoreWebApp/Startup.cs
+++ CoreWebApp/CoreWebApp/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
+using Serilog;
 //using Microsoft.OpenApi.Models;  -> is used in preview version
 
 
