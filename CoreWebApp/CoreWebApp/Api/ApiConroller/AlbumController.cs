40eafe2abe0efcaf53c5da8652b1bce544732748
Dawid Stoga <dawid.stoga@gmail.com>
2019-10-05 20:06:20 +0200

    #ClientFactory: Three ways to use httpClient Factory

---
+++ CoreWebApp/CoreWebApp/Api/ApiConroller/AlbumController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWebApp.Models;
+using CoreWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreWebApp.Api.ApiConroller
+{
+    [Route("api/[controller]")]
+    public class AlbumController : Controller
+    {
+        private readonly IAlbumService _albumService;
+        public AlbumController(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IEnumerable<Album>> Get()
+        {
+            return await _albumService.GetAlbums();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<Album> Get(int id)
+        {
+            return await _albumService.GetAlbumItem(id);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CoreWebApp/CoreWebApp/Services/PhotosServiceWithNamedClient.cs
@@ -0,0 +1,52 @@
+﻿using CoreWebApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CoreWebApp.Services
+{
+    public interface IPhotosService
+    {
+        Task<Photo> GetPhotoItem(int id);
+        Task<IEnumerable<Photo>> GetPhotos();
+    }
+    public class PhotosServiceWithNamedClient : IPhotosService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public PhotosServiceWithNamedClient(IHttpClientFactory httpClientFactory)
+        {
+            _clientFactory = httpClientFactory;
+        }
+        public async Task<Photo> GetPhotoItem(int page)
+        {
+            string _remoteServiceBaseUrl = "https://jsonplaceholder.typicode.com/photos";
+
+            var httpClient = _clientFactory.CreateClient("photos");
+
+
+            var ressponseString = await httpClient.GetStringAsync($"{_remoteServiceBaseUrl}/{page}");
+            var category = JsonConvert.DeserializeObject<Photo>(ressponseString);
+
+            return category;
+        }
+
+        public async Task<IEnumerable<Photo>> GetPhotos()
+        {
+            var httpClient = _clientFactory.CreateClient("photos");
+
+            // create request explicitly
+            var request = new HttpRequestMessage(HttpMethod.Get, "/phptos");
+            var response = await httpClient.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<IEnumerable<Photo>>();
+            }
+
+            return null;
+        }
+    }
+}
