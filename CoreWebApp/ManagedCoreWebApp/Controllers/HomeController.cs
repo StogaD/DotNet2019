304e2b5d11c17e236c94b45ad5c5a7066b608139
Dawid Stoga <dawid.stoga@gmail.com>
2019-10-09 21:51:32 +0200

    #Azure keyvalue - Managed identities for Azure resources

---
+++ CoreWebApp/ManagedCoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ManagedCoreWebApp.Models;
+using Microsoft.Extensions.Options;
+
+namespace ManagedCoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _secret;
+        public HomeController(IOptions<Secret> secrets)
+        {
+            _secret = secrets.Value?.Sekretkey1;
+        }
+        public IActionResult Index()
+        {
+            return View((object)_secret);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoreWebApp/ManagedCoreWebApp/Models/Secret.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagedCoreWebApp.Models
+{
+    public class Secret
+    {
+        public string Sekretkey1 { get; set; }
+    }
+}
